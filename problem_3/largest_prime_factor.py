345fc9075676301ec0a957e7778e2f9930878402
George Richards <georich@protonmail.com>
2019-07-01 18:07:08 +0100

    refactor problem 3 code

--- problem_3/largest_prime_factor.py
+++ problem_3/largest_prime_factor.py
@@ -1,15 +1,20 @@
 from math import sqrt
 
-target = 600851475143
-primes = []
+def is_prime(factor):
+    """Check if a given number is a prime number"""
+    for x in range(factor - 2, 1, -2):
+        if factor % x == 0: # not a prime
+            return False
+    return True
 
-for i in range(3, int(sqrt(target)), 2):
-    if target % i == 0: # is a factor
-        prime = 1
-        for x in range(i - 2, 1, -2):
-            if i % x == 0: # not a prime
-                prime = 0
-        if prime:
-            primes.append(i)
+def largest_prime_factor(target):
+    """Find the largest prime factor of the number given"""
+    primes = []
+    for i in range(3, int(sqrt(target)), 2):
+        if target % i == 0: # is a factor
+            if is_prime(i):
+                primes.append(i)
+    return primes[-1]
 
-print(f"The largest prime factor of {target} is {primes[-1]}")
+number = 600851475143
+print(f"The largest prime factor of {number} is {largest_prime_factor(number)}")
